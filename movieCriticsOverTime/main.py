30b179bc875a5852f9d71f396c0ac78c4e582dfd
Jessicaliu21 <wwenliu@uw.edu>
2019-12-09 18:43:47 -0800

    revise upper case names

--- movieCriticsOverTime/main.py
+++ movieCriticsOverTime/main.py
@@ -9,31 +9,31 @@ import sentiment_analysis
 import visualization
 
 #LOAD DATA
-MOVIES = pd.read_csv("../data/movies.dat", delimiter='\t')
-REVIEWS = pd.read_csv('../data/reviews.csv')
+movies = pd.read_csv("../data/movies.dat", delimiter='\t')
+reviews = pd.read_csv('../data/reviews.csv')
 
 #OVERALL LOOK
 print('Please insert the start year of a range of interest')
 start_year = int(input())
 print('Please insert the end year of a range of interest')
 end_year = int(input())
-SUB_MOVIES = clean_data.clean_movies(MOVIES)
-overall_rating.draw_trend_line_median_year(SUB_MOVIES, start_year, end_year)
+sub_movies = clean_data.clean_movies(MOVIES)
+overall_rating.draw_trend_line_median_year(sub_movies, start_year, end_year)
 
 #CLEAN AND MERGE DATA
-REVIEWS_CLEAN = clean_data.clean_reviews(REVIEWS)
-REVIEWS_MERGE = clean_data.merge_movies_reviews(REVIEWS_CLEAN, SUB_MOVIES)
+reviews_clean = clean_data.clean_reviews(REVIEWS)
+reviews_merge = clean_data.merge_movies_reviews(reviews_clean,sub_movies)
 
 #TOP CRITICS IN YEAR OF INTEREST
 print('Please input a year of interest to see critic activity that year: ')
 interest_year = int(input())
-top_critics = overall_rating.top5_critic_per_year(REVIEWS_MERGE, interest_year)
+top_critics = overall_rating.top5_critic_per_year(reviews_merge, interest_year)
 print('The top critics that year are:')
 print(top_critics)
 
 #GRAB CRITIC REVIEWS
-QUOTES = sentiment_analysis.grab_quotes(REVIEWS_MERGE, top_critics, interest_year)
+quotes = sentiment_analysis.grab_quotes(reviews_merge, top_critics, interest_year)
 
 #ANALYZE QUOTES
-EMOTION_ARRAY = sentiment_analysis.analyze_quote(QUOTES, top_critics)
-visualization.visualize(EMOTION_ARRAY)
+emotion_array = sentiment_analysis.analyze_quote(quotes, top_critics)
+visualization.visualize(emotion_array)
