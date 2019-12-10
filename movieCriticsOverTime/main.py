e9372b7ddcee41c0a158d5f17504c47a8b7c10c2
Jessicaliu21 <wwenliu@uw.edu>
2019-12-09 19:14:07 -0800

    change the names in main file

--- movieCriticsOverTime/main.py
+++ movieCriticsOverTime/main.py
@@ -9,19 +9,21 @@ import sentiment_analysis
 import visualization
 
 #LOAD DATA
-movies = pd.read_csv("../data/movies.dat", delimiter='\t')
-reviews = pd.read_csv('../data/reviews.csv')
+MOVIE_DATA_PATH = "../data/movies.dat"
+REVIEW_DATA_PATH = '../data/reviews.csv'
+movies = pd.read_csv(MOVIE_DATA_PATH, delimiter='\t')
+reviews = pd.read_csv(REVIEW_DATA_PATH)
 
 #OVERALL LOOK
 print('Please insert the start year of a range of interest')
 start_year = int(input())
 print('Please insert the end year of a range of interest')
 end_year = int(input())
-sub_movies = clean_data.clean_movies(MOVIES)
+sub_movies = clean_data.clean_movies(movies)
 overall_rating.draw_trend_line_median_year(sub_movies, start_year, end_year)
 
 #CLEAN AND MERGE DATA
-reviews_clean = clean_data.clean_reviews(REVIEWS)
+reviews_clean = clean_data.clean_reviews(reviews)
 reviews_merge = clean_data.merge_movies_reviews(reviews_clean,sub_movies)
 
 #TOP CRITICS IN YEAR OF INTEREST
