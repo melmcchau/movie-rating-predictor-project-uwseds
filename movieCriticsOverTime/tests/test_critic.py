648f5b12197cbb0272f0e44d5ae04348cf8d1a4b
ppimjqq <jiaminli@uw.edu>
2019-12-09 22:35:00 -0800

    added test

---
+++ movieCriticsOverTime/tests/test_critic.py
@@ -0,0 +1,34 @@
+"""
+Unit tests for checking the availability of data
+"""
+# import sys
+# sys.path.append("..")
+
+import unittest
+from os import path
+
+import pandas as pd
+
+from .. import clean_data, overall_rating, sentiment_analysis
+
+
+class TestViz(unittest.TestCase):
+    """
+    Unit tests for quotes
+    """
+    def test_critic(self):
+        """
+        Checking if the quotes have right structure
+        """
+        movie_data = pd.read_csv(path.join("..", "..", "data", "movies.dat"), delimiter='\t')
+        review_data = pd.read_csv(path.join("..", "..", "data", "reviews.csv"))
+        movie = clean_data.clean_movies(movie_data)
+        review = clean_data.clean_reviews(review_data)
+        movies_reviews = clean_data.merge_movies_reviews(review,movie)
+        top_critics = overall_rating.top5_critic_per_year(movies_reviews, 2000)
+
+        self.assertTrue(type(top_critics) == list)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ movieCriticsOverTime/tests/test_viz.py
@@ -0,0 +1,46 @@
+"""
+Unit tests for checking the availability of data
+"""
+# import sys
+# sys.path.append("..")
+
+import unittest
+from os import path, remove
+
+import pandas as pd
+
+import altair
+
+from .. import clean_data, overall_rating, sentiment_analysis
+
+
+class TestViz(unittest.TestCase):
+    """
+    Unit tests for quotes
+    """
+    def test_viz_output_type(self):
+        """
+        Checking if the quotes have right structure
+        """
+        movie_data = pd.read_csv(path.join("..", "..", "data", "movies.dat"), delimiter='\t')
+        #review_data = pd.read_csv(path.join("..", "..", "data", "reviews.csv"))
+        movies = clean_data.clean_movies(movie_data)
+        a = overall_rating.draw_trend_line_median_year(movies,2003,2010)
+
+        self.assertTrue(type(a) == altair.vegalite.v3.api.Chart)
+        if path.exists('overallchart.html'):
+            remove('overallchart.html')
+
+    def test_html_generated(self):
+        movie_data = pd.read_csv(path.join("..", "..", "data", "movies.dat"), delimiter='\t')
+        movies = clean_data.clean_movies(movie_data)
+        overall_rating.draw_trend_line_median_year(movies,2003,2010)
+
+        self.assertTrue(path.exists('overallchart.html'))
+        remove('overallchart.html')
+        self.assertFalse(path.exists('overallchart.html'))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
